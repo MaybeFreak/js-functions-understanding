c6aa292377f8836b7b6fc4f909e80f0967819527
MaybeFreak <michaelpope97@gmail.com>
2022-11-16 15:19:21 +0100

    finished Q20-25

---
+++ q25.js
@@ -0,0 +1,13 @@
+function myFunction(a) {
+    let b = 20
+    
+    console.log("a:", a)  //100
+    console.log("b:", b)  //20
+    console.log("c:", c)  //3
+}
+  
+let a = 1
+let b = 2
+let c = 3
+  
+myFunction(100, 20)
